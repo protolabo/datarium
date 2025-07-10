290e3a128bf7f4217dda36ff3cee543fdfcf13c9
MoussaDbt <papa.moussa.diabate@umontreal.ca>
2025-07-10 20:10:56 +0200

    .mis a jour effectué sur le script pour rendre la capture plus rapide
    Nouvelles dépendances :
    ipwhois
    json
    ipadress
    
    .ajout d'un autre service efootball (konami) il s'agit d'un jeu

--- sniff1.py
+++ sniff1.py
@@ -1,10 +1,11 @@
-from scapy.all import sniff, IP, TCP, UDP
+from scapy.all import sniff, IP, TCP, UDP, get_if_addr, get_if_list
 import socket
 import re
-import requests
+import socket
+from ipwhois import IPWhois
+import ipaddress
 import json
 
-clé_Api_ipInfo = "10b7032ed7d483"   ## c'est le token de mon compte ipinfo.io, une fois le nbr d'appels les requetes vont echouer, faudra vous creer un compte et mettre votre token ici
 
 services ={
     "YouTube": 0,
@@ -12,29 +13,52 @@ services ={
     "Spotify": 0,
     "TikTok": 0,
     "ChatGPT": 0,
+    "Efootball": 0,
     "Unknown": 0
 }
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+print("Ton ip est : "+ s.getsockname()[0])
+my_ip = s.getsockname()[0]  # l'ip local
+s.close()  
+try:
+    my_hostname = socket.gethostbyaddr(my_ip)[0]
+    print("Ton hostname est : "+ my_hostname)
+except socket.herror:
+    my_hostname = 'Unknown'
 
-
-def get_whois_online(ip):
+# https://scapy.readthedocs.io/en/stable/api/scapy.interfaces.html
+#https://scapy.readthedocs.io/en/latest/routing.html
+interfaces = get_if_list()    
+for iface in interfaces:
     try:
-        r = requests.get(f"https://ipinfo.io/{ip}/json?token={clé_Api_ipInfo}", timeout=2)
-        data = r.json()
-        org = data.get("org", "Unknown org")
-        hostname = data.get("hostname", "No hostname")
-        return {"hostname" : hostname , "org":org}
+        interface_ip = get_if_addr(iface)
+        if interface_ip == my_ip:
+            print(f"Interface trouvée {my_ip} : {iface}")
+            interface = iface
+            break
     except Exception as e:
-        return {"erreur" : str(e), "hostname": "No hostname", "org": "Unknown org"}
+        print(f"Erreur durant la récupération de l'IP pour l'interface {iface}: {e}")
+
+
 
 def get_hostname(ip):
     try:
-        hostname = socket.gethostbyaddr(ip)
-        return {"hostname": hostname[0], "org": "Unknown org"}
-    except Exception as e:
-        return get_whois_online(ip)  ## en cas d'erreur de résolution de nom avec socket j'essaye avec l'API ipinfo
+        name = socket.gethostbyaddr(ip)[0] if ip != my_ip else my_hostname
+    except socket.herror:
+        name = "Unknown"
+    if name != "Unknown":
+        return name
+    else:  
+        try:
+            if not ipaddress.ip_address(ip).is_private and ip != my_ip:
+                who = IPWhois(ip).lookup_rdap()
+                return who.get('asn_description', 'Unknown')
+        except Exception as e:
+            return "Unknown"    
 
-def is_tiktok_domain(ip):
+def is_tiktok_domain(domain):
     tiktok_patterns = [
     r'(^|\.)muscdn\.com$',
     r'(^|\.)musical\.ly$',
@@ -45,42 +69,45 @@ def is_tiktok_domain(ip):
     r'^a\d+\.r\.akamai\.net$',
     r'^[\w\d-]+\.api2\.akamaiedge\.net$',
     ]
-    reponse = get_whois_online(ip)
-    if reponse and reponse.get("hostname") != "No hostname":
-        hostname = reponse["hostname"]
+    if domain and domain != "Unknown":
         for pattern in tiktok_patterns:
-            if re.match(pattern, hostname):
+            if re.match(pattern, domain):
                 return True
-    elif reponse and reponse.get("org"):
-        if (reponse['org'].lower() == "AS396986 Bytedance Inc.".lower()) or (reponse['org'].split()[0].lower()== "AS396986".lower()):
+        if domain.__contains__("TIKTOK") or domain.__contains__("BYTEDANCE"):
+            return True    
+    return False    
+    
+def is_chatgpt(domain):
+    if domain and domain != "Unknown":
+        if domain.__contains__("CHATGPT") or domain.__contains__("OPENAI") or domain.__contains__("CLOUDFLARE"):
             return True
-    else:
-        return False    
-
-def is_spotify(ip):
-    spotify_patterns = [r'^([\w-]+\.)*spotifycdn\.map\.fastly\.net$',
-    r'^([\w-]+\.)*scdn\.co$',
-    r'^([\w-]+\.)*spotify\.com$',
-    r'^([\w-]+\.)*spclient\.wg\.spotify\.com$']
-    reponse = get_whois_online(ip)
-    if reponse and reponse.get("hostname") != "No hostname":
-        hostname = reponse["hostname"]
-        for pattern in spotify_patterns:
-            if re.match(pattern, hostname):
-                return True
-    elif reponse and reponse.get("org"):
-        return (reponse['org'].lower() == "AS54113 Fastly, Inc.".lower()) or (reponse['org'].split()[0].lower()== "AS54113".lower())
-    else:
-        return False
-       
-def is_chatgpt(ip):
-    reponse = get_whois_online(ip)
-    if reponse and reponse.get("org"):
-        org_low = reponse['org'].split()[0].lower()
-        return org_low in ["as13335", "as8075"]
     return False
 
-def is_youtube(ip):
+#https://www.appsruntheworld.com/customers-database/customers/view/konami-digital-entertainment-uk-united-kingdom
+def is_efootball_konami(domain):
+    konami_patterns = [
+    r'^([\w-]+\.)*cloudfront\.net$',
+    r'^([\w-]+\.)*compute\.amazonaws\.com$',
+    ]
+    if domain and domain != "Unknown":
+        for pattern in konami_patterns:
+            if re.match(pattern, domain):
+                return True
+    #pratiquement sur que ca ne sera jamais exécuté car gethostbyaddr renvoie touours une response
+    return False
+                                                                            
+
+def is_netflix(domain):
+    netflix_patterns = [
+    r'^([\w-]+\.)*nflxvideo\.net$',
+    ]
+    if domain and domain != "Unknown":
+        for pattern in netflix_patterns:
+            if re.match(pattern, domain):
+                return True   
+    return False   
+
+def is_youtube(domain):
     youtube_patterns = [
     r'^r\d+\.sn-[\w-]+\.googlevideo\.com$',      
     r'^rr\d+\.sn-[\w-]+\.googlevideo\.com$',     
@@ -90,24 +117,23 @@ def is_youtube(ip):
     r'^m?\.?youtube(-nocookie)?\.com$',          
     r'^youtubei\.googleapis\.com$',              
     ]
-    reponse = get_whois_online(ip)
-    if reponse and reponse.get("hostname") != "No hostname":
-        hostname = reponse["hostname"]
+    if domain and domain != "Unknown":
         for pattern in youtube_patterns:
-            if re.match(pattern, hostname):
+            if re.match(pattern, domain):
                 return True
     return False
 
-def is_netflix(ip):
-    netflix_patterns = [
-    r'^([\w-]+\.)*nflxvideo\.net$',
-    ]
-    reponse = get_whois_online(ip)
-    if reponse and reponse.get("hostname") != "No hostname":
-        hostname = reponse["hostname"]
-        for pattern in netflix_patterns:
-            if re.match(pattern, hostname):
+def is_spotify(domain):
+    spotify_patterns = [r'^([\w-]+\.)*spotifycdn\.map\.fastly\.net$',
+    r'^([\w-]+\.)*scdn\.co$',
+    r'^([\w-]+\.)*spotify\.com$',
+    r'^([\w-]+\.)*spclient\.wg\.spotify\.com$']
+    if domain and domain != "Unknown":
+        for pattern in spotify_patterns:
+            if re.match(pattern, domain):
                 return True
+        if domain.__contains__("FASTLY") or domain.__contains__("SPOTIFY"):
+            return True      
     return False
 
 proto_names = {
@@ -126,22 +152,32 @@ def paquet(packet):
         print(f"src Ip:{src}, src port:{sport} --> dst Ip:{dst}, dst port:{dport}")
         names= get_hostname(src)  ## ici j'utilise les sockets au lieu de get_whois car j'ai juste 50k/mois je limite les appels
         named= get_hostname(dst)  ## mm chose
+        if names != my_hostname and names != "Unknown":
+            domain_p = names
+        else:
+            if named != my_hostname and named != "Unknown":
+                domain_p = named
+            else:
+                domain_p = "Unknown"    
         size = len(packet)
-        if is_tiktok_domain(src) or is_tiktok_domain(dst):
+        if is_tiktok_domain(domain_p):
             print(f"[TikTok] {names} <--> {named}")
             services["TikTok"] += size
-        elif is_spotify(src) or is_spotify(dst):
+        elif is_spotify(domain_p):
             print(f"[Spotify] {names} <--> {named}") 
             services["Spotify"] += size  
-        elif is_chatgpt(src) or is_chatgpt(dst):
+        elif is_chatgpt(domain_p):
             print(f"[ChatGPT] {names} <--> {named}")  
             services["ChatGPT"] += size   
-        elif is_youtube(src) or is_youtube(dst):
+        elif is_youtube(domain_p):
             print(f"[YouTube] {names} <--> {named}")
             services["YouTube"] += size
-        elif is_netflix(src) or is_netflix(dst):
+        elif is_netflix(domain_p):
             print(f"[Netflix] {names} <--> {named}")
             services["Netflix"] += size
+        elif is_efootball_konami(domain_p):
+            print(f"[Efootball] {names} <--> {named}")
+            services["Efootball"] += size    
         else:
             print(f"Unknown {names} <--> {named}")
             services["Unknown"] += size
@@ -156,5 +192,5 @@ def save_services_files():
         json.dump(services, f, indent=4)
     print("le service a été sauvegardé dans le fichier services.json")
 
-sniff(iface=r'\Device\NPF_{4AFAC528-8B21-412C-B7B8-CD188D08F2DE}', filter="(host 192.168.2.62)", prn=paquet, store=False)
+sniff(iface=interface, filter="(host "+my_ip+")", prn=paquet, store=False)
 # iface est l'interface de capture, vous pouvez la changer selon votre réseau 
